d0171506fecb93a4e025a84a4c9a042e87808704
m-moiz <muhammadmoiz856@hotmail.com>
2021-11-01 16:43:55 -0700

    Updates

---
+++ precache-manifest.1c82dff6d1004f447aa8d0ea31fb9d29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d06c541878e341830e843cf75cc93f6",
+    "url": "/wordcloud/index.html"
+  },
+  {
+    "revision": "cc99d8486a1ad92a8f86",
+    "url": "/wordcloud/static/css/main.d3b77e97.chunk.css"
+  },
+  {
+    "revision": "807b09bfe8cd28345f79",
+    "url": "/wordcloud/static/js/2.bd1cae0a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/wordcloud/static/js/2.bd1cae0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc99d8486a1ad92a8f86",
+    "url": "/wordcloud/static/js/main.54236d3a.chunk.js"
+  },
+  {
+    "revision": "d23c4c7ecef36e614258",
+    "url": "/wordcloud/static/js/runtime-main.92531312.js"
+  }
+]);
